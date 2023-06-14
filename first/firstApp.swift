fb46ba68637fa74ae4f077ee9f61bd76b506bdc2
nazilaeshghi <nazila.eshghi@gmail.com>
2023-06-14 10:01:11 +0330

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by Nazila Eshghi on 3/24/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
